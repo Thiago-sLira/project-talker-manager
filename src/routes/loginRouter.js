588a5150e482bc7760d6f3b3294ccd3460217723
Thiago Lira <thiago.slira@outlook.com>
2023-03-14 16:41:12 -0300

    Token concluído

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -5,13 +5,12 @@ const router = express.Router();
 router.use(express.json());
 
 router.post('/', async (request, response) => {
-    const { email, password } = request.body;
+    // const { email, password } = request.body;
     let token = '';
     const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let index = 0; index <= 16; index += 1) {
+    for (let index = 0; index < 16; index += 1) {
         token += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
     }
-    console.log(token);
     return response.status(200).json({ token });
 });
 
